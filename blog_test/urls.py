45a45be1653fca18274175b417ff3e92e2d78bd1
jkxruby <jkxruby@gmail.com>
2017-11-01 14:11:58 +0800

    add index.html

--- blog_test/urls.py
+++ blog_test/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from blog_test_web.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^index/', index),
 ]
+
+
+
