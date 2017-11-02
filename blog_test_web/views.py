53f3655a51fa434f87d3c9c188851923a3bac444
jkxruby <jkxruby@gmail.com>
2017-11-02 21:00:22 +0800

    add the paginator function

--- blog_test_web/views.py
+++ blog_test_web/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from blog_test_web.models import ItemInfo
 
+from django.core.paginator import Paginator
+
 # Create your views here.
 
 def index(request):
+    limit = 4
     item_info = ItemInfo.objects[:1]
+    paginator = Paginator(item_info,limit)
+    page = request.GET.get('page',1)
+    loaded = paginator.page(page)
+
     context ={   # 模拟一个假的数据库.models层相当于一个数据库的代理，把数据库内容映射到templates模版层中，
-        'title': item_info[0].title,
-        'url': item_info[0].url,
-        'price': item_info[0].price
+        'ItemInfo':loaded
 
     }
     return render(request, 'index.html',context)  # 调用
